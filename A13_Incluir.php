6b8a721c22c3a748989a7248cc9c3b6ae93de1e5
unknown <andres@digitalhouse.com>
2018-07-05 20:28:21 -0300

    inclusão dos arquivos do exercicio 3: incluir e comprimento.php

---
+++ A13_Incluir.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exercicio 3</title>
+  </head>
+  <body>
+    <?php
+      echo "eu sou incluir.php";
+      include_once("comprimento.php");
+      include_once("comprimento.php");
+      include_once("comprimento.php");
+    ?>
+  </body>
+</html>
