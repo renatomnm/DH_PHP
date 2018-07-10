c4d00710440f99ed4c1e03f84fcdadcc9dc58383
unknown <andres@digitalhouse.com>
2018-07-10 20:03:22 -0300

    exercicio 2 - Com post

---
+++ Aula14/imprimir2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Exercicio 1 - Requisição GET</h1>
+    <?php
+      foreach($_GET as $parameter){
+        echo $parameter  . "<br>";
+      }
+     ?>
+  </body>
+</html>
