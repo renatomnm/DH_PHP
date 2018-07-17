b75162be6a58dd1f130fb8b02aeb2088d9457b4b
unknown <andres@digitalhouse.com>
2018-07-16 22:35:03 -0300

    resolvendo o exercicio 3 (muito treta!)

--- Aula16/form.php
+++ Aula16/form.php
@@ -13,8 +13,15 @@
           $tamanho = filesize($json);
           $conteudo = fread($arquivo, $tamanho);
           fclose($arquivo);
-          $cat = json_decode($conteudo);
+          $categorias = json_decode($conteudo,true);
       ?>
+          <?php foreach ($categorias as $key => $valores): ?>
+            <?php foreach ($valores as $elementos): ?>
+              <input type="checkbox" name="<?php echo $elementos["nome"]?>" value="<?php echo $elementos["id"]?>">
+              <label for="<?php echo $elementos["nome"]?>"><?php echo $elementos["nome"]?></label>
+              <br>
+            <?php endforeach; ?>
+          <?php endforeach; ?>
     </form>
   </body>
 </html>
