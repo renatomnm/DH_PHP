4da943ce7123db7fb8d41dc4c1f704128903a210
renatomnm <renatomnm@gmail.com>
2018-07-28 20:53:46 -0300

    primeiro commit na nova brancj

--- Aula18/cor.php
+++ Aula18/cor.php
@@ -1,35 +1,26 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<?php
-		$color = "";
-	?>
-	<title>Exercicio 2 - Aula 18</title>
-	<style type="text/css">
-		form {
-			background: <?php echo $color?>;
-		}		
-	</style>
-</head>
-<body>
-	<h2>Exercicio 2 - Aula 18</h2>
-	<?php
-		session_start();
-		if(isset($_POST["Verde"])){
-			$color = "green";
-		} else if(isset($_POST["Vermelho"])) {
-			$color = "red";
-		}
-	?>
-	<form action="#" method="post">	
-		<select name="cores" value="cores">
-		  <option value="Verde">Verde</option>
-		  <option value="Vermelho">Vermelho</option>
-		<select>
-		<input type="submit" name="aplicar">
-	</form>
-	<?php 
+	<head>
+		<?php
+			$color = "";
+			session_start();
+			if(isset($_POST["Verde"])){
+				$color = "green";
+			} else if(isset($_POST["Vermelho"])) {
+				$color = "red";
+			}
+		?>
+		<title>Exercicio 2 - Aula 18</title>
+	</head>
+	<body style='background:<?php echo $color ?>'>
+		<h2>Exercicio 2 - Aula 18</h2>
 
-	?>
-</body>
+		<form action="#" method="post">	
+			<select name="cores" value="cores">
+			  <option value="Verde">Verde</option>
+			  <option value="Vermelho">Vermelho</option>
+			<select>
+			<input type="submit" name="aplicar">
+		</form>
+	</body>
 </html>
